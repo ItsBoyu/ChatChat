7db1a107747cda1c7d49a86e0f69d46b708c7e4d
Boyu <rider945@outlook.com>
2020-04-27 02:21:27 +0800

    copycat

--- app/controllers/chatchat_controller.rb
+++ app/controllers/chatchat_controller.rb
@@ -14,26 +14,52 @@ class ChatchatController < ApplicationController
   end
 
   def webhook
-    # render plain: params
-    # Line Bot API 物件初始化
-    client = Line::Bot::Client.new { |config|
-      config.channel_secret = "92d001c6a3ea46401d239d1c82f1c5f6"
-      config.channel_token = "jEKnpF4gBjr11XJyVDr0+SNgCz52mC2mZKIIfedzlbfn609io61vQPCZThkEMjRK8ZmUavoi32u2wjNGNTgrgf0QsdZec1QJOZEZ63QRaWAx5xnRmg6VRRegiX1oHi1wBdJhjoWkP/xx/qCid8yOLAdB04t89/1O/w1cDnyilFU="
-    }
+    # 核心程式
+    # reply_message = reply(received_message)
 
+    # render plain: params
+    # 設定回覆文字
+    reply_text = keyword_reply(received_text)
+
+    # 傳送訊息到 line
+    response = reply_to_line(reply_text)
+
+    # 回應200
+    head :ok
+  end
+
+  # 取得對方說的話
+  def received_text
+    params["events"][0]["message"]["text"]
+  end
+
+  # 關鍵字回覆
+  def keyword_reply(received_text)
+    received_text
+  end
+
+  # 傳送訊息到 line
+  def reply_to_line(message)
     # 取得 reply token
     reply_token = params["events"][0]["replyToken"]
 
     # 設定回覆訊息
     message = {
       type: "text",
-      text: "我知道了～"
+      text: "我知道了！"
     }
 
     # 傳送訊息
-    response = client.reply_message(reply_token, message)
+    line.reply_message(reply_token, message)
+  end
 
-    # 回應200
-    head :ok
+
+  # Line Bot API 物件初始化
+  def line
+    return @line if not @line.nil?
+    @line = Line::Bot::Client.new { |config|
+      config.channel_secret = "92d001c6a3ea46401d239d1c82f1c5f6"
+      config.channel_token = "jEKnpF4gBjr11XJyVDr0+SNgCz52mC2mZKIIfedzlbfn609io61vQPCZThkEMjRK8ZmUavoi32u2wjNGNTgrgf0QsdZec1QJOZEZ63QRaWAx5xnRmg6VRRegiX1oHi1wBdJhjoWkP/xx/qCid8yOLAdB04t89/1O/w1cDnyilFU="
+    }
   end
 end
