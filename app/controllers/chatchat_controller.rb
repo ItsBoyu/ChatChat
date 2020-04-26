ce905b6c77d879f849c3e2b5d6d19a8d877c3ed3
Boyu <rider945@outlook.com>
2020-04-27 00:46:06 +0800

    add webhook

--- app/controllers/chatchat_controller.rb
+++ app/controllers/chatchat_controller.rb
@@ -1,5 +1,18 @@
 class ChatchatController < ApplicationController
+  
   def eat
     render plain: "這是 Controller"
+  end
+
+  def request_headers
+    render plain: request.headers.to_h.reject{ |key, value| key.include? "." }.map{ |key, value|
+      "#{key} : #{value}"
+      # "#{key} : #{value.class}"
+      # key + " : " + value.class.to_s
+    }.sort.join("\n")
+  end
+
+  def webhook
+    render plain: params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/chatchat/eat", to: "chatchat#eat"
+  get "/chatchat/request_headers", to: "chatchat#request_headers"
+  post "/chatchat/webhook", to: "chatchat#webhook"
 end
