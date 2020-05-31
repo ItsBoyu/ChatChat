5243c74550a71d61bb5040c0567facb862c04e60
Boyu <rider945@outlook.com>
2020-05-31 14:45:17 +0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :keyword_mappings
   get "/chatchat/eat", to: "chatchat#eat"
   get "/chatchat/request_headers", to: "chatchat#request_headers"
   post "/chatchat/webhook", to: "chatchat#webhook"
